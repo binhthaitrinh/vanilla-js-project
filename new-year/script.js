339b83390b3e505fcf6a5b3c6db7bbc7964ca9fe
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2020-01-30 16:53:49 -0800

    add count down functionality

---
+++ new-year/script.js
@@ -0,0 +1,28 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
+
+const newYearTime = new Date('January 01 2021 00:00:00');
+const currentYear = new Date().getFullYear();
+
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? '0' + h : h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+
+  console.log(d);
+  console.log(h, m, s);
+}
+
+setInterval(() => updateCountdown(), 1000);
