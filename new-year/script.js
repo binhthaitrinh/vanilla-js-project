920108f25a59eac207199e6457bfc27a675c2119
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2020-01-30 17:04:20 -0800

    add loading before showing countdown

--- new-year/script.js
+++ new-year/script.js
@@ -3,9 +3,13 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
 
-const newYearTime = new Date('January 01 2021 00:00:00');
 const currentYear = new Date().getFullYear();
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+year.innerHTML = currentYear + 1;
 
 function updateCountdown() {
   const currentTime = new Date();
@@ -18,11 +22,17 @@ function updateCountdown() {
 
   days.innerHTML = d;
   hours.innerHTML = h < 10 ? '0' + h : h;
-  minutes.innerHTML = m;
-  seconds.innerHTML = s;
+  minutes.innerHTML = m < 10 ? '0' + m : m;
+  seconds.innerHTML = s < 10 ? '0' + s : s;
 
   console.log(d);
   console.log(h, m, s);
 }
+
+// show spinner before countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = 'flex';
+}, 1000);
 
 setInterval(() => updateCountdown(), 1000);
